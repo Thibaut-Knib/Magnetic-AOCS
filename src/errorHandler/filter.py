d9032246d46458cd5408a3456b61cc8f9a51b38c
Thibaut KNIBIEHLY <thibaut.knibiehly@gmail.com>
2020-11-30 12:41:52 +0100

    sitll debugging

--- src/errorHandler/filter.py
+++ src/errorHandler/filter.py
@@ -11,11 +11,6 @@ class UKF:
         self.Qcov = Qcov  #Process noise
         self.dt = dt  #time step
 
-    def addition(x,L):  #x is a state(quaternion + rotation) and L is an array(two 3-dim vectors = 6-dim vector)
-        alpha = np.linalg.norm(L[0:3])
-        direction = L[0:3]/alpha
-
-        return [x[0]*Quaternion(np.cos(alpha/2),direction[0,0]*np.sin(alpha/2),direction[1,0]*np.sin(alpha/2),direction[2,0]*np.sin(alpha/2)),x[1] + L[:,3:6]]
 
     def sigmaPoints(self):
         sqrtmatrix = np.linalg.cholesky(self.P + self.Qcov)
@@ -57,7 +52,7 @@ class UKF:
             WiPrime.append(elmt)
         return WiPrime
 
-    def errorCorrection(self, w, B):
+    def errorCorrection(self, WM, BM):
         '''
         Renvoie au pas de temps de l'appel la correction de la mesure
         '''
@@ -69,7 +64,7 @@ class UKF:
         return
 
 def addition(x,L):  #x is a state(quaternion + rotation) and L is an array(two 3-dim vectors = 6-dim vector)
-    alpha = np.linalg.norm(L[:,0:3])
-    direction = L[:,0:3]/alpha
+    alpha = np.linalg.norm(L[0:3])
+    direction = L[0:3]/alpha
 
-    return [x[0]*Quaternion(np.cos(alpha/2),direction[0,0]*np.sin(alpha/2),direction[1,0]*np.sin(alpha/2),direction[2,0]*np.sin(alpha/2)),x[1] + L[:,3:6]]
+    return [x[0]*Quaternion(np.cos(alpha/2),direction[0,0]*np.sin(alpha/2),direction[1,0]*np.sin(alpha/2),direction[2,0]*np.sin(alpha/2)),x[1] + L[3:6]]
