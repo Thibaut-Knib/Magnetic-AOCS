a720ef498157e4af048c6411bf3f5d60ac59413a
Thibaut KNIBIEHLY <thibaut.knibiehly@gmail.com>
2020-11-30 14:01:18 +0100

    debuuug

--- src/errorHandler/filter.py
+++ src/errorHandler/filter.py
@@ -14,16 +14,18 @@ class UKF:
 
     def sigmaPoints(self):
         sqrtmatrix = np.linalg.cholesky(self.P + self.Qcov)
+
         res = []
         for i in range(self.dim):
-            res.append( addition(self.x, sqrt(2*self.dim) * sqrtmatrix[:,i]) )
+            res.append( addition(self.x, sqrt(2*self.dim) * np.atleast_2d(sqrtmatrix[:,i]).T) )
         for i in range(self.dim):
-            res.append( addition(self.x, -sqrt(2*self.dim) * sqrtmatrix[:,i]) )
+            res.append( addition(self.x, -sqrt(2*self.dim) * np.atleast_2d(sqrtmatrix[:,i]).T) )
         return res
 
     def stateMean(self,Yi):
 
         mean = []
+        print(Yi[:][0])
         mean.append(Quaternion.mean(Yi[:][0],1e-4))
         rotMean = np.zeros((3,1))
         for i in range(2*self.dim):
@@ -38,7 +40,7 @@ class UKF:
         for i in range(len(Xi)):
             ajout = np.zeros((6,1))
             ajout[0:3] = Xi[i][1]*self.dt
-            Yi.append(addition(x,ajout))
+            Yi.append(addition(Xi[i],ajout))
         return Yi
 
     def WiCalculus(Yi, xk_):
@@ -57,6 +59,9 @@ class UKF:
         Renvoie au pas de temps de l'appel la correction de la mesure
         '''
         Xi = self.sigmaPoints() # Caclul des Wi, calcul des Xi et sauvegarde dans self.sigPoints
+        #print("\n")
+        #print(Xi)
+        #print("\n")
         Yi = self.evolv(Xi) # process model, le bruit étant intégré dans les sigmaPoints
         xk_ = self.stateMean(Yi)
         WiPrime = WiCalculus(Yi, xk_)
@@ -65,6 +70,10 @@ class UKF:
 
 def addition(x,L):  #x is a state(quaternion + rotation) and L is an array(two 3-dim vectors = 6-dim vector)
     alpha = np.linalg.norm(L[0:3])
-    direction = L[0:3]/alpha
+    if alpha < 1e-4:
+        direction = np.array([[1.0],[0.0],[0.0]])
+    else:
+        direction = L[0:3]/alpha
+
 
     return [x[0]*Quaternion(np.cos(alpha/2),direction[0,0]*np.sin(alpha/2),direction[1,0]*np.sin(alpha/2),direction[2,0]*np.sin(alpha/2)),x[1] + L[3:6]]
--- src/errorHandler/test_UKF.py
+++ src/errorHandler/test_UKF.py
@@ -7,7 +7,7 @@ from scao.quaternion import Quaternion
 
 dim = 6 # 3 pour les quaternions, 3 pour la rotation
 q0 = Quaternion(1,0,0,0)
-W0 = np.array([0,0,0])
+W0 = np.array([[1.0],[0.0],[0.0]])
 P0 = np.eye(dim)
 Qcov = np.eye(dim)
 dt = 1
