236057079e408869d34093594472edb12d61cbf6
Thibaut KNIBIEHLY <thibaut.knibiehly@gmail.com>
2020-11-30 11:16:06 +0100

    update skeletton

--- src/errorHandler/filter.py
+++ src/errorHandler/filter.py
@@ -9,7 +9,7 @@ class UKF:
         self.x = [q0,W0]  #Current state
         self.P = P0  #Covariance matrix on the state
         self.Qcov = Qcov  #Process noise
-        self.sigPoints = None  #List of sigma points
+        self.sigPoints = np.zeros(2*dim)  #List of sigma points (Xi)
         self.dt = dt  #time step
 
     def addition(x,L):  #x is a state(quaternion + rotation) and L is an array(two 3-dim vectors = 6-dim vector)
@@ -21,11 +21,10 @@ class UKF:
     def sigmaPoints(self):
         sqrtmatrix = np.linalg.sqrtm(self.P + self.Qcov)
 
-        self.sigPoints = []
         for i in range(self.dim):
-            self.sigPoints.append(addition(self.x, sqrt(2*self.dim) * sqrtmatrix[:,i]))
+            self.sigPoints[i] = addition(self.x, sqrt(2*self.dim) * sqrtmatrix[:,i])
         for i in range(self.dim):
-            self.sigPoints.append(addition(self.x, -sqrt(2*self.dim) * sqrtmatrix[:,i]))
+            self.sigPoints[i+self.dim] = addition(self.x, -sqrt(2*self.dim) * sqrtmatrix[:,i])
 
     def prediction(self):
         self.sigmaPoints()
@@ -43,9 +42,11 @@ class UKF:
         return addition(x,ajout)
 
 
-    def errorCorrection(w, B):
+    def errorCorrection(self, w, B):
         '''
         Renvoie au pas de temps de l'appel la correction de la mesure
         '''
+        self.sigmaPoints() # Caclul des Wi, calcul des Xi et sauvegarde dans self.sigPoints
+        Yi = self.evolv()
 
         return
